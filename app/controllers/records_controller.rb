40f8ea9b97b10c8a8996025835cc79c5e6266019
kbbushman <k.bushman@yahoo.com>
2016-10-17 17:49:59 -0700

    adds edit, update, new, and delete VCM

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,53 @@
 class RecordsController < ApplicationController
 
 	def index
+		#get all records
 		@records = Record.all
+		#render json: @records (returns json instead of HTML. Could use for JSON API)
 	end
 
 	def show
-		@record = Record.find(params[:id])
+		#show a record by id
+		@record = Record.find(params[:id]) # TODO: handle error
+		render :show #optional
 	end
+
+	def new
+		# create a new instance of a record
+		@record = Record.new
+    render :new #optional
+  end
+
+  def create
+
+    Record.create(record_params)
+    redirect_to records_path #or use 'redirect_to record' to redirect to newly created record
+  end
+
+  def edit
+  	@record = Record.find(params[:id])
+  end
+
+  def update
+  	@record = Record.find(params[:id])
+  	@record.update(record_params)
+  	redirect_to @record
+  end
+
+  def destroy
+  	record = Record.find(params[:id])
+  	record.destroy
+  	redirect_to records_path
+  end
+
+  private
+
+  # Below are "strong parameters" used for security purposes
+  
+
+  def record_params
+    params.require(:record).permit(:title, :artist, :year, :cover_art, :song_count)
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #http verb + path, controller#action, alias
-  get "/records/:id", to: "records#show", as: "record"
-  get "/records", to: "records#index", as: "records"
+  root "records#index"
+
+  # get "/records", to: "records#index", as: "records"
+  # get "/records/new", to: "records#new", as: 'new_record'
+  # get "/records/:id", to: "records#show", as: "record"
+  # post "/records", to: "records#create"
+  # delete "/records/:id", to: "records#destroy"
+  # get "/records/:id", to: "records#edit"
+  # patch "/records/:id", to: "records#update"
+  # put "/records/:id", to: "records#update"
+
+  resources :records
+
 end
